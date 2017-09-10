125bb73ef92691399687987e8c8ced54093f5d82
David Dassau <todieahero@hotmail.com>
2017-09-10 15:20:11 -0500

    added new buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,6 +9,44 @@ document.getElementById("fancyPetStore").addEventListener("click", (event) => {
 });
 
 
+
+
+
+	// <button class="btn btn-warning" type="submit" id="all">All Cats</button>
+	// <button class="btn btn-danger" type="submit" id="homeless">Homeless Kats</button>
+	// <button class="btn btn-info" type="submit" id="zoe">Zoe's Kats</button>
+	// <button class="btn btn-warning" type="submit" id="callan">Callan's Kats</button>
+	// <button class="btn btn-success" type="submit" id="lauren">Lauren's Kats</button>
+
+document.getElementById("all").addEventListener("click", (event) => {
+	let cats = CatStore.getCats();
+	let owners = CatStore.getOwners();
+	CatStore.combineArrays(cats, owners);
+})
+
+// For Callan's Cats Button
 document.getElementById("callan").addEventListener("click", (event) => {
 	CatStore.getCatsByOwner(2);
-})
+});
+
+
+// For Zoe's Cats Button
+document.getElementById("zoe").addEventListener("click", (event) => {
+	CatStore.getCatsByOwner(1);
+});
+
+
+// For Lauren's Cats Button
+document.getElementById("lauren").addEventListener("click", (event) => {
+	CatStore.getCatsByOwner(3);
+});
+
+
+// For All Cats Button
+document.getElementById("all").addEventListener("click", (event) => {
+	CatStore.getCatsByOwner(3);
+});
+
+
+
+
